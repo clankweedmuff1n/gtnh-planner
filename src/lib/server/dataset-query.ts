0370e6dddceeff890be3b02b2779fd882cb645f5
Sami MOURAD <mourads2001@gmail.com>
2026-05-28 12:18:18 +0200

    Broaden recipe tier filter

--- src/lib/server/dataset-query.ts
+++ src/lib/server/dataset-query.ts
@@ -1605,9 +1605,7 @@ function recipeMatchesTierIndex(indexes: QueryIndexes, recipeIndex: number, maxT
   if (maxTier === "all") {
     return true;
   }
-  return (
-    (indexes.tierIndexes[recipeIndex] ?? GT_VOLTAGE_TIERS.length - 1) === getTierIndex(maxTier)
-  );
+  return (indexes.tierIndexes[recipeIndex] ?? GT_VOLTAGE_TIERS.length - 1) <= getTierIndex(maxTier);
 }
 
 function recipeMatchesLookupTier(
@@ -1618,7 +1616,7 @@ function recipeMatchesLookupTier(
   if (maxTier === "all") {
     return true;
   }
-  return (lookup.tierIndexes[recipeIndex] ?? GT_VOLTAGE_TIERS.length - 1) === getTierIndex(maxTier);
+  return (lookup.tierIndexes[recipeIndex] ?? GT_VOLTAGE_TIERS.length - 1) <= getTierIndex(maxTier);
 }
 
 function getRecipeTier(recipe: RecipeSummary): Exclude<MachineTier, "DEMO"> {

cd8bd2ab8703518d44921cb29e7d18896a2913cd
Sami MOURAD <mourads2001@gmail.com>
2026-06-01 15:17:58 +0200

    Make dataset startup prewarm opt-in

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,6 +3,10 @@ export async function register() {
     return;
   }
 
+  if (process.env.GTNH_PREWARM_ON_STARTUP !== "1") {
+    return;
+  }
+
   const { prewarmLatestDatasetVersions } = await import("@/lib/server/dataset-query");
   try {
     await prewarmLatestDatasetVersions();
